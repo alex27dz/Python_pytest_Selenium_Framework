14a2894ed751f8e925aeeb941b2c86bc6f5d2535
“alex27dz” <alex27dz@gmail.com>
2023-09-17 13:44:39 -0400

    updated

--- Functionality/func_syntax_examples.py
+++ Functionality/func_syntax_examples.py
@@ -6,6 +6,7 @@ def variables():
     age = 25
     height = 1.75
     is_student = True
+
 def strings():
     string = 'example'
     substring = 'example2'
@@ -26,6 +27,7 @@ def strings():
     string.isnumeric()  # Returns True if the string consists of only numeric characters.
     string.isalpha()  # Returns True if the string consists of only alphabetic characters.
     string.isalnum()  # Returns True if the string consists of only alphanumeric characters.
+
 def lists():
     lista = [5, 2, 3]
     print(lista)
@@ -50,6 +52,7 @@ def lists():
     listb.clear()  # Removes all elements from the list
     iterable = 'a'
     listb.extend(iterable)  # Appends the elements of an iterable to the end of the list.
+
 def dictionary():
     fruits = {
         "apple": 1,
@@ -76,6 +79,7 @@ def dictionary():
     print(fruits)  # Output: {}
     person = {"name": "Alice", "age": 25, "city": "New York"}
     print(person["name"])
+
 def email_send():
     import yagmail
 
@@ -91,8 +95,6 @@ def email_send():
         print('email sent')
 
     send_email(email, subject, body)
-# email_send()
-
 
 def linux_bash_commands():
     '''
@@ -129,6 +131,162 @@ def linux_bash_commands():
     history: Display the command history.
     man: Display the manual page of a command.
     '''
+
+def built_in_funcs():
+    # Built-in functions
+    my_list1 = [1, 2, 3]
+    my_list2 = [1, 2, 3, 4, 5]
+    is_list1 = isinstance(my_list1, list)
+    is_list2 = isinstance(my_list2, list)
+    print(is_list1)  # Output: True
+    print(is_list2)  # Output: True
+    # list()
+    my_tuple = (1, 2, 3)
+    my_list = list(my_tuple)
+    print(my_list)  # Output: [1, 2, 3]
+    # abs()
+    number = -10
+    absolute_value = abs(number)
+    print(absolute_value)  # Output: 10
+    # bin()
+    number = 10
+    binary_string = bin(number)
+    print(binary_string)  # Output: '0b1010'
+    # exec()  - executing the code inside the parameter as string
+    code = """
+    for i in range(5):
+        print(i)
+    """
+    exec(code)  # Output:# 0# 1# 2# 3# 4
+    # float()
+    number = '3.14'
+    float_number = float(number)
+    print(float_number)  # Output: 3.14
+    # format()
+    name = "Alice"
+    age = 25
+    formatted_string = "My name is {} and I'm {} years old".format(name, age)
+    print(formatted_string)  # Output: My name is Alice, and I'm 25 years old
+    # globals()
+    global_variable = 10
+    global_variables = globals()
+    print(global_variables['global_variable'])  # Output: 10
+    # hex()
+    number = 255
+    hex_string = hex(number)
+    print(hex_string)  # Output: '0xff'
+    # id()
+    my_list = [1, 2, 3]
+    list_id = id(my_list)
+    print(list_id)  # Output: [a unique identifier]
+    # input()
+    # name = input("Enter your name: ")
+    print("Hello, " + name)  # Output: Hello, [name]
+    # int()
+    number = '10'
+    integer_number = int(number)
+    print(integer_number)  # Output: 10
+    # len()
+    my_list2 = [1, 2, 3, 4, 5]
+    length = len(my_list2)
+    print(length)  # Output: 5
+    # list()
+    my_tuple = (1, 2, 3)
+    my_list = list(my_tuple)
+    print(my_list)  # Output: [1, 2, 3]
+    # max()
+    numbers = [4, 2, 7, 1, 5]
+    max_number = max(numbers)
+    print(max_number)  # Output: 7
+    # min()
+    numbers = [4, 2, 7, 1, 5]
+    min_number = min(numbers)
+    print(min_number)  # Output: 1
+    # open()
+    file = open("data.txt", "r")
+    contents = file.read()
+    print(contents)
+    file.close()
+    # reversed()
+    my_list = [1, 2, 3]
+    reversed_list = list(reversed(my_list))
+    print(reversed_list)  # Output: [3, 2, 1]
+    # round()
+    value = 3.14159
+    rounded_value = round(value, 2)
+    print(rounded_value)  # Output: 3.14
+    # slice()
+    my_list = [1, 2, 3, 4, 5]
+    sliced_list = my_list[1:4]
+    print(sliced_list)  # Output: [2, 3, 4]
+    # sorted()
+    numbers = [4, 2, 7, 1, 5]
+    sorted_numbers = sorted(numbers)
+    print(sorted_numbers)  # Output: [1, 2, 4, 5, 7]
+    # str()
+    number = 10
+    string_number = str(number)
+    print(string_number)  # Output: '10'
+    # sum()
+    my_list = [1, 2, 3, 4, 5]
+    total = sum(my_list)
+    print(total)  # Output: 15
+    # tuple()
+    my_list = [1, 2, 3]
+    my_tuple = tuple(my_list)
+    print(my_tuple)  # Output: (1, 2, 3)
+    # type()
+    my_object = object()
+    print(type(my_object))  # Output: <class 'object'>
+    # zip()
+    numbers = [1, 2, 3]
+    letters = ['A', 'B', 'C']
+    zipped = zip(numbers, letters)
+    for item in zipped:
+        print(item)  # Output: # (1, 'A')# (2, 'B')# (3, 'C')
+
+
+def if_statement():
+    # if statements
+    age = 19
+    if age > 18:
+        print('adult')
+    else:
+        print('child')
+
+def for_while_loops():
+    # For Loops
+    for i in range(1, 10):
+        print(i, 'for loop')
+
+    name = 'alex'
+    for i in name:
+        print(i, 'i in string')
+    i = 0
+
+    while i < 10:
+        print(i, 'while loop')
+        i += 1
+
+
+def break_continue():
+    # Control Flow
+    for i in range(1, 10):
+        if i == 5:
+            break
+        print(i, 'control flow')
+    for i in range(1, 10):
+        if i == 5:
+            continue
+        print(i)
+
+
+
+
+
+
+
+
 
 
 
